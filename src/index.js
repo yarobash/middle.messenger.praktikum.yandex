63759e294116c2979aefef4a9f611e6473f643b5
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-08-26 21:29:29 +0300

    refactor(proj): change project to precompiling layouts

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-import './index.hbs';
-import './pages/login/login.hbs';
-import './pages/chat/chat.hbs';
+import handlebars from 'handlebars/dist/handlebars.runtime';
+
+import indexPage from './pages/index/index.hbs';
+import chatPage from './pages/chat/chat.hbs';
+import loginPage from './pages/login/login.hbs';
+
+import outMsgPart from './components/out-message/outMessage.hbs';
+import inMsgPart from './components/in-message/inMessage.hbs';
+
+import loginPageContext from './shared/contexts/login';
+import chatPageContext from './shared/contexts/chat';
+
+handlebars.registerPartial('outMsgPart', outMsgPart);
+handlebars.registerPartial('inMsgPart', inMsgPart);
+
+const content = document.querySelector('.content');
+
+content.innerHTML = indexPage();
+
+switch (window.location.pathname) {
+  case '/login':
+    content.innerHTML = loginPage(loginPageContext);
+    break;
+  case '/chat':
+    content.innerHTML = chatPage(chatPageContext);
+    break;
+  default:
+    content.innerHTML = indexPage();
+} 
---
+++ src/shared/contexts/login.js
@@ -0,0 +1,4 @@
+export default {
+  id: "shroot",
+  laika: "Panda Laika",
+}
