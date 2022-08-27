a6dc51b60f47884b28dd5e64e19bdf5621fb135e
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-08-27 18:36:58 +0300

    feat(messages): add date stamp

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import msgImgPart from './components/msg-img/msgImg.hbs';
 import textingForm from './components/texting-form/textingForm.hbs';
 import curUserPart from './components/cur-user/curUser.hbs';
 import chatManagePart from './components/chat-manage/chatManage.hbs';
+import chatDatePart from './components/chat-date/chatDate.hbs';
 
 import loginPageContext from './shared/contexts/login';
 import chatPageContext from './shared/contexts/chat';
@@ -20,6 +21,7 @@ handlebars.registerPartial('msgImgPart', msgImgPart);
 handlebars.registerPartial('textingForm', textingForm);
 handlebars.registerPartial('curUserPart', curUserPart);
 handlebars.registerPartial('chatManagePart', chatManagePart);
+handlebars.registerPartial('chatDatePart', chatDatePart);
 
 const content = document.querySelector('.content');
 
